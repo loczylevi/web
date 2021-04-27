7a863f6fcea410290032bf0e4eb5303ff0119759
Lóczy Levente Attila <loczy2003@gmail.com>
2021-04-27 11:41:55 +0200

    9. Nyissa meg a kalkulal.js állományt, módosítsa a függvényt a következők szerint! a. A letszam, az ejszaka és az orszag változók értékeit állítsa be az űrlap megfelelő mezői alapján!

--- kalkulal.js
+++ kalkulal.js
@@ -1,12 +1,18 @@
 function szamol() {
-    var letszam = 1;
-    var ejszaka = 1;
-    var orszag = 'b';
+    var letszam = document.getFlementById("letszam").value;
+    var ejszaka = document.getFlementById("ejszaka").value;
+    var orszag = document.getFlementById("orszag").value;
     var fizetendo;
+    var ar = {};
+    ar["b"] = 5600;
+    ar["g"] = 6200;
+    ar["h"] = 5000;
+    ar["t"] = 5800;
 
     //--számolás--
-      
+      fizetendo = letszam * ejszaka * ar[orszag];
+
     //------------
-    document.getElementById('eredmeny').value = fizetendo+" Ft";
+    document.getElementById('eredmeny').value = fizetendo.toLocaleString()+" Ft";
     
 }
